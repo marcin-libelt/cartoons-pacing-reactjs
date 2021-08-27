1a248e00834cf716902fc7439accc2a1c0818071
Marcin Libelt <m.libelt@alekseon.com>
2021-08-27 16:51:56 +0200

    MBITV-61 comments tooltip

--- src/components/Box.js
+++ src/components/Box.js
@@ -1,6 +1,8 @@
 import React, {memo, useState} from 'react';
 import { useDrag } from 'react-dnd';
 import { qtyReducer } from '../helper';
+import ReactTooltip from 'react-tooltip';
+
 const style = {
     border: '1px dashed gray',
     backgroundColor: 'white',
@@ -20,6 +22,7 @@ export const Box = memo(function Box({ name, hidden, type, id, doorLabel, PO, do
                                          unit_selling_price,
                                          warehouseLocation, isDropped }) {
 
+    const comment = sizes[0].comments !== "" ? sizes[0].comments : null;
     const [{ opacity }, drag] = useDrag(() => ({
         type,
         item: { id, doorLabel, doorCode },
@@ -42,13 +45,25 @@ export const Box = memo(function Box({ name, hidden, type, id, doorLabel, PO, do
                     <span className={'label'} style={labelStyle}>PO number:</span>{PO}<br/>
                     <span className={'label'} style={labelStyle}>SO number:</span>{joorSONumber}<br/>
                 </p>
-                <p style={{ width: '60%', lineHeight: '1.5'}} className={'m-0'}>
+                <p style={{ width: '60%', lineHeight: '1.5', position: 'relative'}} className={'m-0'}>
                     <span className={'label'} style={labelStyle}>door label:</span>{doorLabel}<br/>
                     <span className={'label'} style={labelStyle}>client name:</span>{clientName}
                     &nbsp;/&nbsp;<strong style={{color: warehouseLocation === 'VAT' ? '#8e6009' : '#1b1fb9'}}>{warehouseLocation}</strong><br/>
                     <span className={'label'} style={labelStyle}>order type:</span>{orderType}<br/>
                     <span className={'label'} style={labelStyle}>unit price:</span>{parseFloat(unit_selling_price) + ' €'}
+                    { comment ? <a
+                        data-for={"tooltip-" + id}
+                        data-tip={comment}
+                        style={{position: 'absolute', bottom: 0, right: 0}}
+                    >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="#03962b"
+                             className="bi bi-info-circle-fill" viewBox="0 0 16 16">
+                            <path
+                                d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16zm.93-9.412-1 4.705c-.07.34.029.533.304.533.194 0 .487-.07.686-.246l-.088.416c-.287.346-.92.598-1.465.598-.703 0-1.002-.422-.808-1.319l.738-3.468c.064-.293.006-.399-.287-.47l-.451-.081.082-.381 2.29-.287zM8 5.5a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+                        </svg>
+                    </a>: ''}
                 </p>
+                { comment ? <ReactTooltip id={"tooltip-" + id} multiline={true} /> : ''}
             </div>
         </div>
         <div className={'card-body p-3'}>
